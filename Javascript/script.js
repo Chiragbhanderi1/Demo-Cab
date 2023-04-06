750f85ac3a8631543b59d558b81e441e1a273195
Chirag <cbhanderi666@gmail.com>
2023-04-06 21:20:58 +0530

    major

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,3 @@
-
 // for active tab on navbar
 const activePage = window.location.pathname;
 const navlink  = document.querySelectorAll('nav ul li a').
@@ -8,39 +7,3 @@ forEach(link=>{
     };
 });
 // active tab on navbar ends here 
-
-// google map in location picker
-// var map;
-// var marker;
-// var searchBox;
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: 37.7749, lng: -122.4194},
-//     zoom: 13
-//   });
-//   marker = new google.maps.Marker({
-//     map: map,
-//     position: map.getCenter()
-//   });
-//   searchBox = new google.maps.places.SearchBox(document.getElementById('search-box'));
-//   searchBox.addListener('places_changed', function() {
-//     var places = searchBox.getPlaces();
-//     if (places.length == 0) {
-//       return;
-//     }
-//     var bounds = new google.maps.LatLngBounds();
-//     places.forEach(function(place) {
-//       if (!place.geometry) {
-//         console.log("Returned place contains no geometry");
-//         return;
-//       }
-//       if (place.geometry.viewport) {
-//         bounds.union(place.geometry.viewport);
-//       } else {
-//         bounds.extend(place.geometry.location);
-//       }
-//     });
-//     map.fitBounds(bounds);
-//     marker.setPosition(map.getCenter());
-//   });
-// }
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const ejs = require('ejs');
 const fs = require('fs');
 const admin = require('firebase-admin');
 const bodyParser = require('body-parser')
+const twilio = require('twilio');
+const mustacheExpress = require('mustache-express');
 // Initialize Firebase Admin SDK
 const serviceAccount = require('./key.json');
 admin.initializeApp({ 
@@ -11,9 +13,16 @@ admin.initializeApp({
   databaseURL: 'https://cab-rental-6445a-default-rtdb.asia-southeast1.firebasedatabase.app/'
 });
 
-// Set EJS as the view engine
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+const client = twilio("AC5379cc509dd22a03dec92142cb441d5d","2c8f13abd180ca6d8849fc5fd3fda461");
+
+// // Set EJS as the view engine
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
+
+// Configure Mustache as the templating engine
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app.use(express.static(__dirname + "/public/"));
 
@@ -30,22 +39,24 @@ app.get('/about', (req, res) => {
   res.sendFile(__dirname + '/about.html');
 });
 
-app.get('/contact', (req, res) => {
+app.get('/contact', (req, res) => {  
   res.sendFile(__dirname + '/contact.html');
 });
 
 app.get('/book', (req, res) => {
   res.sendFile(__dirname + '/book.html');
 });
+app.get('/packages', (req, res) => {
+  res.sendFile(__dirname + '/packages.html');
+}); 
 
 app.get('/fleets', async(req, res) => { 
-  const data  = await db.collection('cab').get();
+  const data  = await db.collection('cab').get(); 
   const documents = [];
   data.docs.forEach((doc) => {
     documents.push(doc.data()); 
   });
-  console.log(documents)
-  res.render("fleets",{documents})
+  res.render("fleets",{documents})  
   // res.sendFile(__dirname + '/fleets.html');
 });
 // form submiting 
@@ -56,15 +67,23 @@ app.post('/submit', async(req, res) => {
   const from = req.body.from
   const to = req.body.to 
   const date = req.body.date
-  const self = req.body.selfdrive || "" 
-  const car = req.body.car || ""
-  const data = {name:name,email:email,phone:phone,from:from,to:to,date:date,self:self,car:car}
+  const car = req.body.car
+  const data = {name:name,email:email,phone:phone,from:from,to:to,date:date,car:car}
   const response = await db.collection("booking").doc().set(data)
   // process the form data here
+  client.messages.create({
+    to: '+919998252323',
+    from: '+14406368399',
+    body: name+" has Booked a cab from "+from+" to "+to+" on "+date +" of car " +car+" his phone Number is"+phone
+  })
+  .then(message => console.log(message.sid))
+  .catch(err => console.error(err));
+  
   if(response){
     res.sendFile(__dirname + '/index.html');
   }
 })
+
 // Start the server
 app.listen(3000, () => {
   console.log('Server started on port 3000');
